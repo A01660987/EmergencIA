70c7ce1b95cb17710bdafb0578bc7aedc551b17e
Diego Figueroa Peart <a01660987@tec.mx>
2023-10-27 17:28:53 -0600

    Renombrar proyecto a EmergencIA

--- EmergencIA/wsgi.py
+++ EmergencIA/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for EmergenceAI project.
+WSGI config for EmergencIA project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EmergenceAI.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EmergencIA.settings')
 
 application = get_wsgi_application()
