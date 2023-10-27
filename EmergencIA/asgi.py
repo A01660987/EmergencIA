6c0f8a1815f48573d4fe7a6e47b8e8fb0e0e08b4
feraranas <46a12sFAASfaas>
2023-10-27 17:40:52 -0600

    websocket

--- EmergencIA/asgi.py
+++ EmergencIA/asgi.py
@@ -8,9 +8,19 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
 import os
-
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+import web.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EmergencIA.settings')
 
-application = get_asgi_application()
+
+application = ProtocolTypeRouter({
+ "http": get_asgi_application(),
+ "websocket": AuthMiddlewareStack(
+       URLRouter(
+           web.routing.websocket_urlpatterns
+       )
+   ),
+})
