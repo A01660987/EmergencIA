9a982041dab367d5470a427fbc47c9bf9eab1176
Diego Figueroa Peart <a01660987@tec.mx>
2023-10-28 09:20:01 -0600

    Update views.py

--- web/views.py
+++ web/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from django.shortcuts import render 
 from django.http import JsonResponse 
+from django.views.decorators.csrf import csrf_exempt
 import openai
 import os
 from dotenv import load_dotenv
